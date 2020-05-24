c4b224e5d71389c8eb5bf2c99c21587ce896d4af
tim-musifullin <terrykane@protonmail.com>
2020-05-24 14:05:06 +0300

    fixed

--- src/js/modules/GitHubApi.js
+++ src/js/modules/GitHubApi.js
@@ -9,10 +9,12 @@ export class GitHubApi {
         "https://api.github.com/repos/tim-musifullin/diploma-news-analyzer/commits?sha=master&per_page=20",
         {
           method: "GET",
+          cors: false
         }
       );
       if (!res.ok) return;
       const data = await res.json();
+      console.log(data);
       return data;
     } catch (error) {
       throw new Error(`Ошибка при запросе к серверу`);
